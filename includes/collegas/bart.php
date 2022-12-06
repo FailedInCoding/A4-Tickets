3f8298e37db9a336cbd4d16f19623d17ff7d8c0d
Sjoerd Pelzer <sjoerd.pelzer@gmail.com>
2022-12-06 09:01:09 +0100

    init

---
+++ includes/collegas/bart.php
@@ -0,0 +1,6 @@
+<div class="collega">
+	<img src="img/profile_pictures/br10.jfif" alt="br10">
+	<h4>Bart Roos </h4>
+	<p>docent WIN/WEB/SLB/CGO</p>
+	<a href="mailto:b.roos@curio.nl">b.roos@curio.nl</a>
+</div>
---
+++ includes/collegas/ine.php
@@ -0,0 +1,6 @@
+<div class="collega">
+	<img src="img/profile_pictures/ib81.jfif" alt="ib81">
+	<h4>Ine Vermeer </h4>
+	<p>docent WEB/SLB (heeft verlof t/m april)</p>
+	<a href="mailto:e.vermeer@curio.nl">e.vermeer@curio.nl</a>
+</div>
